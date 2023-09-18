d08ddef80dd08e6ab40db4f86eb4fe2e59b50e46
Angel-L-G <angelchiquitopons@gmail.com>
2023-09-18 11:11:02 +0100

    feat: guessSecretNumber

---
+++ node/adivinarNum/app.js
@@ -0,0 +1,15 @@
+var toBeGuessed= parseInt(Math.random() * 10);
+
+function adivinar(num){
+    console.log(toBeGuessed);
+    let p = document.getElementById("res");
+    if(num == toBeGuessed){
+        window.alert("Acertaste, bien por ti.");
+        window.location.reload();
+    }else if(num > toBeGuessed){
+        p.innerHTML += `</br>${num} es mayor que el numero oculto`;
+    }else{
+        p.innerHTML += `</br>${num} es menor que el numero oculto`;    }
+
+    
+}
