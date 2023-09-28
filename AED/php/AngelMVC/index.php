d86922130714e4a429deee1e26f772b8b4637612
Angel-L-G <113530247+Angel-L-G@users.noreply.github.com>
2023-09-28 17:19:31 +0100

    feat(PGL): trabajo de hoy;

--- AED/php/AngelMVC/index.php
+++ AED/php/AngelMVC/index.php
@@ -1,3 +1,12 @@
 <?php
     echo "Hola a todos! soy Angel Luis Garcia";
+    $url = strtolower($_REQUEST["pathtocontroller"]);
+    $urlSplitted = explode("/",$url);
+    $urlSplitted[0] = ucfirst($urlSplitted[0])."Controller";
+    $nombreClase = $urlSplitted[0];
+    echo "<br>";
+    $fullUrl = "./app/controller/".$urlSplitted[0].".php";
+    echo "Ruta: ".$fullUrl;
+    echo "<br/>";
+    echo "Clase: ".$nombreClase;
 ?>
