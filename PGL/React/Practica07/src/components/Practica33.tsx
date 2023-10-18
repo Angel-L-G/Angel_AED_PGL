1ec4add0221b8b50e8c4e7a51d18384ce1fb314c
Angel-L-G <angelchiquitopons@gmail.com>
2023-10-18 17:12:49 +0100

    feat(PGL): 33 almost done

--- PGL/React/Practica07/src/components/Practica33.tsx
+++ PGL/React/Practica07/src/components/Practica33.tsx
@@ -1,20 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 type Props = {}
 
 const Practica33 = (props: Props) => {
-    function processForm(){
+    const [listaPrimos, setListaPrimos] = useState([]);
+
+    function processForm(evt: React.FormEvent<HTMLFormElement>){
+        evt.preventDefault();
+
+        let form = evt.currentTarget;
+        let mayor = Number(form.mayor.value);
+        let menor = Number(form.menor.value);
+        
+        let arrAux = [];
+
+        for (let i = menor; i < mayor; i++) {
+            let isPrimo:boolean = true;
+            
+            for (let j = 2; j < i; j++) {
+                if(!(i % j === 0)){
+                    isPrimo = true
+                    break;
+                }
+            }
+            
+            if(isPrimo){
+                arrAux.push(i);
+            }
+        }
 
     }
 
     return (
         <div className='container'>
-            <form action="" method="post">
+            <form onSubmit={processForm}>
                 <label>Primo Mayor</label>
-                <input type="text"/>
+                <input type="text" id="mayor"/>
                 <br/>
                 <label>Primo Menor</label>
-                <input type="text"/>
+                <input type="text" id="menor"/>
                 <br/>
                 <button>Send</button>
             </form>
