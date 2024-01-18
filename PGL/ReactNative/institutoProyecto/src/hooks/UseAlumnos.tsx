3d23325538fc9cd67ed46d7ca7d038197f571234
Angel-L-G <angelchiquitopons@gmail.com>
2024-01-18 20:19:23 +0000

    feat(PGL): start working on the instituto front

---
+++ PGL/ReactNative/institutoProyecto/src/hooks/UseAlumnos.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const UseAlumnos = (props: Props) => {
+  return (
+    <View>
+      <Text>UseAlumnos</Text>
+    </View>
+  )
+}
+
+export default UseAlumnos
---
+++ PGL/ReactNative/institutoProyecto/src/hooks/UseSesion.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const UseSesion = (props: Props) => {
+  return (
+    <View>
+      <Text>UseSesion</Text>
+    </View>
+  )
+}
+
+export default UseSesion
