6c8a83ca2a9e4ea8253cf0173e60db0a58bc32d4
Angel-L-G <angelchiquitopons@gmail.com>
2024-01-19 13:03:32 +0000

    feat(PGL): work on matriculas

--- PGL/ReactNative/institutoBueno/src/hooks/UseAlumnos.tsx
+++ PGL/ReactNative/institutoBueno/src/hooks/UseAlumnos.tsx
@@ -1,14 +1,69 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import { Alumno } from '../components/types';
 
-type Props = {}
+type Props = {
+    navigation: any
+}
 
-const UseAlumnos = (props: Props) => {
-  return (
-    <View>
-      <Text>UseAlumnos</Text>
-    </View>
-  )
+const UseAlumnos = ({navigation}: Props) => {
+    const ruta = "http://localhost:8080/api/v2/alumnos";
+    const [alumnos, setAlumnos] = useState<Array<Alumno>>(Array<Alumno>);
+
+    useEffect(() => {
+        const axiosFindAll = async (ruta: string) => {
+            try{
+                const response = await axios.get(ruta);
+                setAlumnos(response.data);
+                //console.log(peliculas.data);
+            } catch (error){
+                console.log(error);
+            }
+        }
+        
+        axiosFindAll(ruta);      
+    }, [])
+    
+    async function saveAlumno(dni: string, nombre: string, apellidos: string, foto: string, fechanacimiento: number){
+
+        const alumno: Alumno = {
+            dni: dni,
+            nombre: nombre,
+            apellidos: apellidos,
+            foto: foto,
+            fechanacimiento: fechanacimiento
+        };
+
+        const axiospost = async (ruta: string) => {
+            try{
+                const response = await axios.post(ruta, alumno);
+                //console.log(response.data);
+                navigation.navigate("/ListarAlumnos");
+            } catch (error){
+                console.log(error);
+            }
+        }
+
+        axiospost(ruta);
+    }
+
+    async function findAlumnoById(dni: string){
+        try{
+            const c = await axios.get(ruta+"/"+dni);
+            return c.data;
+        } catch (error){
+            console.log(error);
+        }
+        return null;
+    }
+
+    return {
+        saveAlumno,
+        alumnos,
+        findAlumnoById,
+        
+    }
 }
 
 export default UseAlumnos
--- PGL/ReactNative/institutoBueno/src/hooks/UseSesion.tsx
+++ PGL/ReactNative/institutoBueno/src/hooks/UseSesion.tsx
@@ -1,14 +1,62 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import axios from 'axios';
+import { Usuario } from '../components/types';
 
-type Props = {}
+type Props = {
+  navigation:any
+}
 
-const UseSesion = (props: Props) => {
-  return (
-    <View>
-      <Text>UseSesion</Text>
-    </View>
-  )
+const UseSesion = ({navigation}: Props) => {
+  const rutaRegister = "http://localhost:8080/api/register";
+  const rutaLogin = "http://localhost:8080/api/login";
+
+    async function register(nick: string, password: string, email: string){
+
+        let user: Usuario = {
+            email: email,
+            nombre: nick,
+            password: password
+        }
+
+        const axiospost = async (ruta: string) => {
+            try{
+                const response = await axios.post(ruta, user);
+                console.log(response.data);
+            } catch (error){
+                console.log(error);
+            }
+        }
+
+        axiospost(rutaRegister);
+    }
+
+    async function login(nick: string, password: string){
+
+        let user: Usuario = {
+            email: "",
+            nombre: nick,
+            password: password
+        }
+
+        const axiospost = async (ruta: string) => {
+            try{
+                const response = await axios.post(ruta, user);
+                console.log(response.data);
+                //localStorage.setItem('token', response.data);
+                //setToken(response.data);
+            } catch (error){
+                console.log(error);
+            }
+        }
+
+        axiospost(rutaLogin);
+    }
+
+  return {
+    login,
+    register,
+  }
 }
 
 export default UseSesion
