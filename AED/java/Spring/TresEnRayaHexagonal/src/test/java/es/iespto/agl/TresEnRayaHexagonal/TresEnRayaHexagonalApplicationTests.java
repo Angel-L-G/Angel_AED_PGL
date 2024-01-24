1679fd9008bede8f07801bd87d5d02176884685d
Angel-L-G <angelchiquitopons@gmail.com>
2024-01-24 20:29:49 +0000

    feat(AED): project init form hexagonal

---
+++ AED/java/Spring/TresEnRayaHexagonal/src/test/java/es/iespto/agl/TresEnRayaHexagonal/TresEnRayaHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package es.iespto.agl.TresEnRayaHexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TresEnRayaHexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
